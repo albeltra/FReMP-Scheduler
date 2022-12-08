ceb2327f8a88c2a58dc411964a57e5ec7d9ee3c4
Alex Beltran <alexbeltran@berkeley.edu>
2022-12-07 18:58:47 -0800

    change request parsing for debugging

--- flaskserver/scripts/app.py
+++ flaskserver/scripts/app.py
@@ -50,9 +50,9 @@ def token_required(f):
 @app.route('/flask/login', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        print(request.body)
-        user = request.body.get("user")
-        password = request.body.get("password")
+        print(request.form) 
+        user = request.form.get("user")
+        password = request.form.get("password")
 
         token = jwt.encode({'user': user, 'password': password}, app.config['SECRET_KEY'], 'HS256')
         return make_response(jsonify({'token': token}), 201)
